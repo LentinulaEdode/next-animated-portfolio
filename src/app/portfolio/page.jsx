2f98a2b85a085a8da872a35493478be732836eb3
LentinulaEdode <carmona.a.esther@gmail.com>
2024-03-05 17:00:03 +0100

    preparación a deploy

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,12 +1,7 @@
 "use client";    
-import {motion, useScroll, useTransform} from 'framer-motion';
-import {useRef} from 'react';
+import {motion} from 'framer-motion';
 
 const PortfolioPage = () => {
-    const ref = useRef();
-
-    const {scrollYProgress} = useScroll({target:ref});
-    const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
 
     return (
@@ -16,8 +11,8 @@ const PortfolioPage = () => {
             animate={{y: "0%" }} 
             transition={{ duration: 0.6 }}
         >
-        <div className="h-[600vh]" ref={ref}>
-            <div className="w-screen h-calc[(100vh-6rem)] flex items-center justify-center text-4xl text-center">My Works</div>
+        <div className="">
+            <div className="flex items-center justify-center text-4xl text-center">My Works</div>
         </div>
         </motion.div>
     );
